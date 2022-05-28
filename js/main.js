8f9b39a7aac618911f45464614d305168fbe69cb
synth Eater <90665040+SynthEater@users.noreply.github.com>
2022-05-28 16:00:23 -0400

    platforms moving

--- js/main.js
+++ js/main.js
@@ -1,20 +1,20 @@
 let wnX = window.innerWidth;
 let wnY = window.innerHeight;
 let plat = [];
-let nbPlat = 6;
+let nbPlat = 20;
 
 function setup() {
     createCanvas(wnX, wnY);
 
     //generating platforms
     for (let i = 1; i < nbPlat; i++) {
-        plat[i] = new Plat((i * 10) + (wnX - 39), 100);
+        plat[i] = new Plat((i * 100) + (wnX - 39), 100);
     }
 }
 
 function draw() {
     background(51);
-    for (let j = 0; j < nbPlat; j++) {
+    for (let j = 1; j < nbPlat; j++) {
         /////////////////////////////////////////
         plat[j].render();
         plat[j].move();
---
+++ js/player.js
@@ -0,0 +1,6 @@
+class Player {
+    constructor(x, y) {
+
+
+    }
+}
