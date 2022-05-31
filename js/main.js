9cb546ac4555d654c7b5ed8e43791d8d4ecfe6dd
synth Eater <90665040+SynthEater@users.noreply.github.com>
2022-05-31 12:26:40 -0400

    scrapped ground class

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-let wnX = window.innerWidth;
-let wnY = window.innerHeight;
+const wnX = window.innerWidth;
+const wnY = window.innerHeight;
 
 //platforms variables
 let plat = [];
@@ -18,11 +18,17 @@ function setup() {
     createCanvas(wnX, wnY);
 
     //generating platforms
-    for (let i = 1; i < nbPlat; i++) {
-        plat[i] = new Plat((((i * ecart) + (wnX - 39)) - wnX + startTime), random(300, 420));
+    for (let i = 0; i < nbPlat; i++) {
+
+        plat.push(new Plat(((((i + 1) * ecart) + (wnX - 39)) - wnX + startTime), random(300, 420)));
     }
-    //generating ground
-    ground = new Ground(0, wnY - 200, levelWidth, wnY - 200);
+
+    //generating ground with a platform
+    plat[nbPlat] = new Plat(0, wnY - 200);
+
+    plat[nbPlat].width = levelWidth;
+    plat[nbPlat].height = wnY - 200;
+
 
     //generating player
     player = new Player(50, wnY - 236);
@@ -31,13 +37,11 @@ function setup() {
 function draw() {
     background(51);
 
-    ground.render();
+    //ground.render();
     player.render();
 
-    for (let j = 1; j < nbPlat; j++) {
-        plat[j].render();
-        plat[j].move();
+    for (let i = 1; i < plat.length; i++) {
+        plat[i].render();
+        plat[i].move();
     }
-
-
 }
